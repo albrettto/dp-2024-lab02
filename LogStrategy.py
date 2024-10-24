a857704a544f86db722b94ef877cdc8cbd1adcd2
Хусамов Альберт <a.husamov@yandex.ru>
2024-10-25 00:09:31 +0500

    feat(LogStrategy): Реализована логика Логгера
    Реализована логика Логгера с использованием паттернов Singleton и Strategy

---
+++ LogStrategy.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+import os
+
+
+class LogStrategy(ABC):
+    @abstractmethod
+    def write(self, message: str):
+        pass
+
+    @abstractmethod
+    def set_file_path(self, file_path: str):
+        pass
+
+    @staticmethod
+    def get_timestamp() -> str:
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    @staticmethod
+    def get_filename() -> str:
+        return f"DP.P1.{datetime.now().strftime("%Y-%m-%d.%H-%M-%S")}.log"
+
+
+class ConsoleLogStrategy(LogStrategy):
+    def set_file_path(self, file_path: str):
+        pass
+
+    def write(self, message: str):
+        print(message)
+
+
+class FileLogStrategy(LogStrategy):
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def set_file_path(self, file_path: str):
+        self.file_path = file_path
+
+    def write(self, message: str):
+        log_file_path = os.path.join(self.file_path, self.get_filename())
+        with open(log_file_path, 'a') as log_file:
+            log_file.write(message + '\n')
+
+class UpperCaseFileLogStrategy(FileLogStrategy):
+    def set_file_path(self, file_path: str):
+        super().set_file_path(file_path)
+
+    def write(self, message: str) -> None:
+        super().write(message.upper())
+
+
+""" Другой вариант решения """
+# class UpperCaseFileLogStrategy(LogStrategy):
+#     def __init__(self, file_path: str):
+#         self.file_path = file_path
+#
+#     def set_file_path(self, file_path: str):
+#         self.file_path = file_path
+#
+#     def write(self, message: str):
+#         log_file_path = os.path.join(self.file_path, self.get_filename())
+#         with open(log_file_path, 'a') as log_file:
+#             log_file.write(message.upper() + '\n')
+
---
+++ Logger.py
@@ -0,0 +1,23 @@
+from LogStrategy import LogStrategy
+from Singleton import Singleton
+import threading
+
+class Logger(Singleton):
+    _lock = threading.Lock()
+
+    def __init__(self, log_strategy: LogStrategy):
+        self.log_strategy = log_strategy
+
+    def set_log_strategy(self, log_strategy: LogStrategy):
+        self.log_strategy = log_strategy
+
+    def set_file_path(self, file_path: str):
+        self.log_strategy.set_file_path(file_path)
+
+    def _format_message(self, level: str, message: str) -> str:
+        return f"{LogStrategy.get_timestamp()} [{level}] {message}"
+
+    def log(self, level: str, message: str):
+        formatted_message = self._format_message(level, message)
+        with self._lock:
+            self.log_strategy.write(formatted_message)
